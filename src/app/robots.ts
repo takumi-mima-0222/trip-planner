591e369ec53fd18fe6b209feabc63e1d997979d9
takumi_mima <takumi20010222@icloud.com>
2026-01-19 18:04:58 +0900

    url変更

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://tabikumi.com';
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://tabikumi.vercel.app';
 
 /**
  * robots.txt を動的に生成
